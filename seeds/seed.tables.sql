7e80f288878dce1c43c33cc86b2e37864d2846f7
Elijah Simpson <elijahsimpson1991@gmail.com>
2021-02-08 22:53:09 -0500

    changed seed table values

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -10,19 +10,25 @@ VALUES
         1,
         'admin',
         'testtest123!'
-    ),
-
-    (
-        2,
-        'test',
-        'thisismypass123!'
     );
 
-INSERT INTO "tips" ("tip_total", "user_id")
+
+INSERT INTO "tips" (tip_total, user_id)
 VALUES
     (3.20, 1),
     (4.89, 1),
     (1.56, 1),
-    (2.00, 2);
+    (2.00, 1),
+    (3.89, 1),
+    (7.26, 1),
+    (1.25, 1),
+    (3.02, 1),
+    (0.48, 1),
+    (2.15, 1),
+    (1.48, 1),
+    (0.18, 1),
+    (8.99, 1),
+    (1.87, 1),
+    (3.26, 1);
 
 COMMIT;
